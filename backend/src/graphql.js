2f22a8ade33a69c4e1f98520e25317177fc077b5
Marvin Rösch <marvinroesch99@gmail.com>
2019-12-28 16:45:01 +0100

    Expose server channels via GraphQL

--- backend/src/graphql.js
+++ backend/src/graphql.js
@@ -1,7 +1,7 @@
 const graphqlHTTP = require('express-graphql');
 const DataLoader = require('dataloader');
 const { Server, Channel } = require('./db');
-const { checkAuth } = require('./auth');
+const { checkAuth, getPermissions } = require('./auth');
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
@@ -33,26 +33,39 @@ const schema = buildSchema(`
 
 function initLoaders(user) {
   const channelLoader = new DataLoader(async ids => {
-    const servers = await Channel.findAll({ where: { id: ids } });
-    return ids.map(id => servers.find(s => s.id === id) || null);
+    const channels = await Channel.findAll({ where: { id: ids } });
+    return ids.map(id => channels.find(s => s.id === id) || null);
   });
 
   function loadServerChannels(server) {
-    return () => {
-      const ids = user.getPermissions(server.id);
+    if (server === undefined) {
+      return null;
+    }
+
+    server.channels = async () => {
+      const permissions = await getPermissions(user, server.id);
+      const ids = Object.keys(permissions).filter(id => permissions[id].has('readMessages'));
+
+      return await channelLoader.loadMany(ids);
     };
+
+    return server;
   }
 
   return {
     servers: new DataLoader(async ids => {
       const servers = await Server.findAll({ where: { active: true, id: ids } });
-      return ids.map(id => servers.find(s => s.id === id) || null);
+      return ids.map(id => loadServerChannels(servers.find(s => s.id === id)));
     }),
     channels: channelLoader,
   };
 }
 
 const root = {
+  identity(args, request) {
+    const { id, username, discriminator } = request.user;
+    return { id, name: username, discriminator };
+  },
   async servers(args, request) {
     return (await request.loaders.servers.loadMany(request.user.servers.map(server => server.id))).filter(s => s !== null);
   },

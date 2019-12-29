f1fbf28cd8dc89af865379fd7bcca642e219d00c
Marvin Rösch <marvinroesch99@gmail.com>
2019-12-29 21:57:47 +0100

    Prevent double redirect for apollo failure

--- frontend/apollo/error-handler.js
+++ frontend/apollo/error-handler.js
@@ -1,9 +1,15 @@
 export default (error, context) => {
+  if (!error.networkError || error.networkError.statusCode !== 401) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+  }
+  if (context.req.handledApolloError) {
+    return
+  }
   if (error.networkError && error.networkError.statusCode === 401) {
     context.redirect(`/api/auth/login?redirect=${encodeURIComponent(context.route.fullPath)}`)
   } else {
-    // eslint-disable-next-line no-console
-    console.error(error)
     context.error({ statusCode: 500, message: 'Server error' })
   }
+  context.req.handledApolloError = true
 }
